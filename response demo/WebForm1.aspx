a11f17a184c9696bcce75cb5c29af43c56e715d2
student <student@MB>
2023-09-11 12:32:57 +0530

    Add project files.

---
+++ response demo/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="response_demo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" style="top: 148px; left: 257px; position: absolute; height: 26px; width: 56px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ response demo/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace response_demo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for(int i=0;i<100;i++)
+            {
+                Response.Write("<img src=green.jpg>");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<marquee><h2>Welcome to first website</h2></marquee>");
+        }
+    }
+}
